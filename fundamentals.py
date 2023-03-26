1031f43ac7454544f34ffbe5a47de806b0da3c1c
GabrielGQA <gabrielgqa@gmail.com>
2023-03-26 12:25:52 -0300

    feat: fundamentals

--- fundamentals.py
+++ fundamentals.py
@@ -1,4 +1,20 @@
 # Mostra texto na tela
 print("Hello World")
 # Pede para o usuário inserir algum valor
-input("Informe seu nome")
+name = input("Informe seu nome")
+# 'F' de format. Habilita a inserção de variável
+print(f"Bem vindo, {name}!")
+# Variáveis fracamente tipadas
+nome = 'Gabriel'
+new_name = nome
+# Desaloca valor da memória
+del new_name
+# Pegar tipo de variável
+print(type(nome))
+# Valores do boolean com maiúsculo
+booll = False
+print(type(booll))
+# Tipo str/int/float/bool
+# Python -i (abre arquivo no modo interativo usando a função abaixo trás todas as formas de interação disponíveis)
+dir(nome)
+# Conectivos lógicos and, or, not
